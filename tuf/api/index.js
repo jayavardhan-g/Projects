be3f3478c1f24de3fc225867e4bd56f8390e897d
jayavardhan <jayavardhan.gollapalli@gmail.com>
2024-08-13 09:06:43 +0530

    tuf

--- tuf/api/index.js
+++ tuf/api/index.js
@@ -4,6 +4,7 @@ const cors = require('cors');
 const mysql = require('mysql2')
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({extended:true}));
 
 const mysqluri= "mysql://avnadmin:AVNS_SK3s2qzq__67QyqwEEY@tuf-1234-itsmellucas-1234.k.aivencloud.com:28395/tuf?ssl-mode=REQUIRED"
 
@@ -18,47 +19,10 @@ app.listen(3000,()=>{
     console.log(`Listening on port 3000`);
 });
 
-// app.get('/test',(req,res)=>{
-//     connection.query("SELECT VERSION()",(error,rows,fields)=>{
-//         if(error){
-//             console.log(error);
-//             res.send(error);
-//         }
-//         else{
-//             console.log(rows);
-//             console.log("successful");
-//             res.send("Success");
-//         }
-//     })
-// })
-
-// app.post('/currentState',(req,res)=>{
-//     console.log(req.body);
-//     let query = `select * from Banner`;
-//     connection.query(query, (error,rows,fields)=>{
-//         if(error){
-//             console.log(error);
-//             res.send(error);
-//         }
-//         else{
-//             console.log(rows);
-//             console.log(fields);
-//             console.log("successful");
-//             res.send("Success");
-//         }
-//     } )
-// })
-
 app.post('/update',(req,res)=>{
 
-    console.log(req.body);
-    console.log(date);
-    console.log(on);
-    console.log(title)
-    console.log(link);
+    const {date,link,on,title}= req.body;
 
-    // date = `${date}`
-    // link = `${link}`
     const query =`UPDATE Banner SET BannerTitle='${title}', BannerTime='${date}', BannerVisibility=${on}, BannerLink='${link}' WHERE BannerId=1`
 
     connection.query(query,(error,rows,fields)=>{
@@ -68,8 +32,9 @@ app.post('/update',(req,res)=>{
         }
         else{
             console.log(rows);
-            console.log(fields);
+            // console.log(fields);
             // console.log("successful");
+            res.status(200);
             res.send("Success");
         }
     })
@@ -91,25 +56,10 @@ app.get('/getRows',(req,res)=>{
 
             res.send(rows);
         }
-    })
+    })  
 })
 
-// app.get('/create',(req,res)=>{
-//     // let table = req.params.table;
-
-//     const que = `ALTER TABLE Banner CHANGE BannerImage BannerTitle varchar(255);`
-
-//     connection.query(que,(error,rows,fields)=>{
-//         if(error){
-//             console.log(error);
-//             res.send(error);
-//         }
-//         else{
-//             console.log(rows);
-//             console.log(fields);
-//             console.log("successful");
-//             res.send("Success");
-//         }
-//     })
-
-// })
+app.post('/test',(req,res)=>{
+    console.log(req);
+    res.json(req.body);
+})
